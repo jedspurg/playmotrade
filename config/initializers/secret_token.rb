858a3cead5d2244eaaeea4054dd80520701568b6
root <root@playmotrade.com>
2013-08-31 18:13:51 +0000

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Playmotrade::Application.config.secret_key_base = '9724b5c1444aa62817ac5852604e7a1ec325cd4e4a781284a374449a661e3080d47081ecf89872409dc88112c7ab4098ad548b3c47a44c95467308b701fd1bb1'
