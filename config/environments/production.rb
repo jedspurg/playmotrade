f87d380bfa608001fe8fefe856177eef9f48c3b9
Jeramy Spurgeon <jedspurg@iupui.edu>
2013-09-01 16:51:44 -0400

    can we get it back?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Playmotrade::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  Paperclip.options[:command_path] = "/usr/bin/convert"
+  #Paperclip.options[:command_path] = "/usr/bin/convert"
 
-  config.action_mailer.default_url_options = { :host => 'playmotrade.com' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
