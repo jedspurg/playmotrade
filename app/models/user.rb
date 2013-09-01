0dc615f01dfaa8705feccfe63eb59e87e9485a9e
Jeramy Spurgeon <jedspurg@iupui.edu>
2013-09-01 18:06:22 -0400

    better errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :email, :presence => true, :uniqueness => true
   validates :username, :presence => true, :uniqueness => true
-  validates :password, :presence => true,
-                    :length => { :minimum => 5, :maximum => 40 },
-                    :confirmation =>true
   validates :agrees_to_tos, :presence => true
   scope :active, -> { where(:active => true) }
 end
