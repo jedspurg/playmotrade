64f07a5f9f22a1285b583d88e0df7a29bb2f074c
Jeramy Spurgeon <jedspurg@iupui.edu>
2013-09-01 15:41:04 -0400

    User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,45 @@
 class UsersController < ApplicationController
 
   def index
-  	@users = User.all #scope this to active users eventually
+    @users = User.active
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def new
-  	@user = User.new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "Account created"
+      redirect_to users_path
+    else
+      render :action => :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "Account updated"
+    end
+    render :action => :edit
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      flash[:notice] = "Account deleted"
+    end
+    redirect_to users_path
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
-  validates :email, :presence =>true, :uniqueness=>true
-  validates :password, :presence =>true,
+  validates :email, :presence => true, :uniqueness => true
+  validates :username, :presence => true, :uniqueness => true
+  validates :password, :presence => true,
                     :length => { :minimum => 5, :maximum => 40 },
                     :confirmation =>true
+
+  scope :active, -> { where(:active => true) }
 end
