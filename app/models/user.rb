5f33f5150e1ef4fff861b4d2aa257cf948ee07e4
Jeramy Spurgeon <jedspurg@iupui.edu>
2013-09-01 17:06:48 -0400

    add devise back

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  #devise :database_authenticatable, :registerable,
-         #:recoverable, :rememberable, :trackable, :validatable
   validates :email, :presence => true, :uniqueness => true
   validates :username, :presence => true, :uniqueness => true
   validates :password, :presence => true,
