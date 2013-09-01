e1d1695d90d26470c67e54935e2d39f6841cf783
Jeramy Spurgeon <jedspurg@iupui.edu>
2013-09-01 15:15:33 -0400

    fix migration

--- db/migrate/20130901185402_change_country_on_user.rb
+++ db/migrate/20130901185402_change_country_on_user.rb
@@ -1,7 +1,7 @@
 class ChangeCountryOnUser < ActiveRecord::Migration
   def change
   	 reversible do |dir|
-      change_table :userss do |t|
+      change_table :users do |t|
         dir.up   { t.change :country, :integer }
         dir.down { t.change :country, :string }
       end
