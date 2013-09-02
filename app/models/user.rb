7bbb17a726319bb4b9e56404d9b8a67809c04a68
Jeramy Spurgeon <jedspurg@iupui.edu>
2013-09-02 08:50:20 -0400

    fix message

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, :presence => true, :uniqueness => true
-  validates :agrees_to_tos, acceptance: true, :message => "You must accept the terms of service."
+  validates :agrees_to_tos, acceptance: true, message: "You must accept the terms of service."
 
   attr_accessible :avatar
   has_attached_file :avatar, :styles => {:medium => "250x250>",
