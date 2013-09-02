3f2eea9938bb4edf7b7ab5b521d1fafb681c0ca8
root <root@playmotrade.com>
2013-09-02 22:40:04 +0000

    confirm users migration

---
+++ db/migrate/20130902223932_confirm_users.rb
@@ -0,0 +1,4 @@
+class ConfirmUsers < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130901203000) do
+ActiveRecord::Schema.define(version: 20130902123655) do
 
   create_table "users", force: true do |t|
     t.string   "email"
@@ -21,10 +21,7 @@ ActiveRecord::Schema.define(version: 20130901203000) do
     t.string   "city"
     t.string   "state"
     t.string   "zip"
-    t.string   "language_preference"
-    t.string   "timezone"
     t.string   "username"
-    t.string   "password"
     t.boolean  "agrees_to_tos"
     t.text     "about"
     t.datetime "created_at"
@@ -42,6 +39,10 @@ ActiveRecord::Schema.define(version: 20130901203000) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
