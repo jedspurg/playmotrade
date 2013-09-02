c88417fd2ac2a98429e77f60f2ef7e447d8c87c4
Jeramy Spurgeon <jedspurg@iupui.edu>
2013-09-02 09:58:43 -0400

    fix acceptance

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, :presence => true, :uniqueness => true
-  validates :agrees_to_tos, acceptance: true
+  validates :agrees_to_tos, :acceptance => {:accept => true}
 
   has_attached_file :avatar, :styles => {:medium => "250x250>",
   				                               :thumb => "100x100>",
