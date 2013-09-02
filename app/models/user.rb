9d1a91f0e9556499e759c88094a6923e682efb1e
Jeramy Spurgeon <jedspurg@iupui.edu>
2013-09-02 08:48:14 -0400

    user avatars and tos validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, :presence => true, :uniqueness => true
-  validates :agrees_to_tos, :presence => true
+  validates :agrees_to_tos, acceptance: true, :message => "You must accept the terms of service."
+
+  attr_accessible :avatar
+  has_attached_file :avatar, :styles => {:medium => "250x250>",
+  				                               :thumb => "100x100>",
+                                         :small => "20x20>" }, 
+                              :default_url => "/images/:style/missing.png"
+
   scope :active, -> { where(:active => true) }
 end
