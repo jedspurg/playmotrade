b1abd1129e2e98a3b28547463158b4211b5d12b2
Jeramy Spurgeon <jedspurg@iupui.edu>
2013-10-22 19:24:28 -0400

    More UI Tweaking

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,7 +1,8 @@
 class CatalogController < ApplicationController
 
   def index
-    @catalog_items = CatalogPart.all.paginate(:page => params[:page], :per_page => 24)
+    @recent_catalog_parts = CatalogItem.where(:catalogable_type => 'CatalogPart').order("created_at DESC").take(6)
+    @recent_catalog_sets = CatalogItem.where(:catalogable_type => 'CatalogSet').order("created_at DESC").take(6)
   end
 
 end
