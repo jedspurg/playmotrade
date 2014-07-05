01aed6931a34cc82dce073d1dab712b4d1386023
Jeramy Spurgeon <jeramy.d.spurgeon@gmail.com>
2014-07-05 14:50:02 -0400

    Modify Order and Items

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   belongs_to :store
-  #has_many :order_items
+  has_many :order_items
+
+  validates :user, :store, :order_items, presence: true
 end
---
+++ db/migrate/20140705183547_modify_order_and.rb
@@ -0,0 +1,14 @@
+class ModifyOrderAnd < ActiveRecord::Migration
+  def change
+    remove_column :order_items, :store_inventory_item_type
+
+    add_column :orders, :status, :string, :null => false, :default => 'open'
+    add_column :orders, :created_at, :datetime, :null => false
+    add_column :orders, :updated_at, :datetime, :null => false
+    add_column :order_items, :created_at, :datetime, :null => false
+    add_column :order_items, :updated_at, :datetime, :null => false
+    add_column :order_items, :catalog_item_type, :string, :null => false
+    add_column :order_items, :catalog_item_id, :integer, :null => false
+    add_column :order_items, :quantity, :integer, :null => false
+  end
+end
