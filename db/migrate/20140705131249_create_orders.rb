7da2b0364d8b1868e5c738dcfb43c8337fe9cc79
Jeramy Spurgeon <jeramy.d.spurgeon@gmail.com>
2014-07-05 09:58:12 -0400

    missing migration

--- db/migrate/20140705131249_create_orders.rb
+++ db/migrate/20140705131249_create_orders.rb
@@ -1,6 +1,18 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
+      t.money :total
+      t.money :shipping
+      t.references :store
+      t.references :user
+    end
+
+    create_table :order_items do |t|
+      t.money :price
+      t.references :store
+      t.references :order
+      t.integer :store_inventory_item_id
+      t.string :store_inventory_item_type
     end
   end
 end
