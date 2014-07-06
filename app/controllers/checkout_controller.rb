1d7a770b47f4ba9d06db0f364092bd063f1d158e
Jeramy Spurgeon <jeramy.d.spurgeon@gmail.com>
2014-07-06 18:19:47 -0400

    Force ssl only on checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,4 +1,10 @@
 class CheckoutController < ApplicationController
+  force_ssl if: :ssl_configured?
+
+  def ssl_configured?
+    !Rails.env.development?
+  end
+
   before_filter :setup_cart
 
   def new
