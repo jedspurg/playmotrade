1ef336b0bf1eae79ec7f1a20260dc74e20e78543
Jeramy Spurgeon <jeramy.d.spurgeon@gmail.com>
2014-07-07 15:01:55 -0400

    Orders index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,16 @@
 class OrdersController < ApplicationController
 
   def index
+    @store = Store.find_by(id: params[:store_id].to_i)
+    @orders = if @store.present?
+      current_user.orders.by_store_id(@store.id).open.paginate(:page => params[:page], :per_page => 24)
+    else
+      current_user.orders.open.paginate(:page => params[:page], :per_page => 24)
+    end
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,23 @@
 class Order < ActiveRecord::Base
+
+  VALID_STATES   = ['open', 'pending shipment', 'canceled', 'shipped', 'completed']
+  OPEN_STATES    = ['open', 'pending shipment', 'shipped']
+  CLOSED_STATES  = ['canceled', 'completed']
+  STATUS_CLASSES = {
+    :open => 'label-info',
+    :pending_shipment => 'label-warning',
+    :canceled => 'label-important',
+    :shipped => 'label-inverse',
+    :completed => 'label-success'
+  }
+
   belongs_to :user
   belongs_to :store
   has_many :order_items, :dependent => :destroy
 
-  VALID_STATES = ['open', 'pending shipment', 'canceled', 'shipped', 'completed']
+  scope :open, -> { where(:status => OPEN_STATES) }
+  scope :closed, -> { where(:status => CLOSED_STATES) }
+  scope :by_store_id, lambda { |store_id| where(:store_id => store_id)}
 
   validates :user, :store, :order_items, presence: true
   validates :order_items, presence: true, on: :update
@@ -12,4 +26,16 @@ class Order < ActiveRecord::Base
   monetize :total_cents
   monetize :shipping_cents
   monetize :application_fee_cents
+
+  def total_items
+    order_items.map(&:quantity).inject { |sum, n| sum + n }
+  end
+
+  def subtotal
+    order_items.map(&:price).inject { |sum, n| sum + n }
+  end
+
+  def status_class
+    STATUS_CLASSES[status.underscore.to_sym]
+  end
 end
